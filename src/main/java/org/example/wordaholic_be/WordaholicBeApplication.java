e3cac9e904e0efcbdd02ebda6ac999098ec7f3a2
SE171343_LuuKimHoang <70700909+HoangKim0312@users.noreply.github.com>
2024-09-25 14:24:48 +0700

    This is the first commit

---
+++ src/main/java/org/example/wordaholic_be/WordaholicBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.wordaholic_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordaholicBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordaholicBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/wordaholic_be/WordaholicBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wordaholic_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordaholicBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
